d7c3d71f626a158abe9fffee520ee287d9528eed
NandiniSannareddy <150068076+NandiniSannareddy@users.noreply.github.com>
2024-04-07 12:43:35 +0530

    Add files via upload

---
+++ log.php
@@ -0,0 +1,23 @@
+<?php
+
+$connect= mysqli_connect('localhost', 'root', '', 'form');
+
+if($connect==true){
+	echo "connectionm successful";
+}
+else
+  echo "error"; 
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+   <?php
+echo "string";
+?>
+</body>
+</html>
